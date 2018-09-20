faefa26f255869c1579fcec8b75860dcfe77134f
Mindsers <dev@nathanaelcherrier.com>
2018-09-20 08:21:10 +0200

    Add sort method

--- src/core/nativetable.class.js
+++ src/core/nativetable.class.js
@@ -1,6 +1,30 @@
 import { FilterApplicator } from './filter-applicator.class.js'
+import { Sorter } from './sorter.class.js'
 
 export class Nativetable {
+  /**
+   * Data sources
+   * Getter
+   *
+   * @return {Object[]}
+   */
+  get sources() {
+    return this.data.sources
+  }
+
+  /**
+   * Data sources
+   * Setter
+   *
+   * @return {Object[]}
+   */
+  set sources(rows) {
+    this.options.reloading.filtered = true
+    this.options.reloading.sorted = true
+    this.options.reloading.paginated = true
+    this.data.sources = rows
+  }
+
   /**
    * Data sources filtered by filters
    * Getter
@@ -9,6 +33,16 @@ export class Nativetable {
    */
   get filtered() {
     return this.data.filtered
+  }
+
+  /**
+   * Data sources sorted
+   * Getter
+   *
+   * @return {Object[]}
+   */
+  get sorted() {
+    return this.data.sorted
   }
 
   /**
@@ -41,7 +75,7 @@ export class Nativetable {
   }
 
   /**
-   * Apply filters on source.
+   * Apply filters on sources.
    * Fill `.filtered` property.
    *
    * @return {Object[]} - The source filtered
@@ -69,5 +103,41 @@ export class Nativetable {
 
     this.options.reloading.filtered = false
     return this.data.filtered
+  }
+
+  /**
+   * Sort sources
+   * Fill `.sorted` property.
+   *
+   * @return {Object[]}
+   */
+  sort() {
+    if ( // pagination cached
+      this.data.sorted &&
+      this.data.sorted.length > 0 &&
+      this.options.reloading.sorted === false
+    ) {
+      return this.data.sorted
+    }
+
+    const sources = this.filtered
+    const { column, order = 'none', activated: isSortingActivated } = this.options.sorting
+
+    if (
+      sources.length <= 0 ||
+      isSortingActivated === false ||
+      typeof column === 'undefined'
+    ) { // no sources or no sorting
+      this.data.sorted = sources
+
+      this.options.reloading.sorted = false
+      return this.data.sorted
+    }
+
+    const sorter = new Sorter()
+    this.data.sorted = sorter.sort(sources, column, order)
+
+    this.options.reloading.sorted = false
+    return this.data.sorted
   }
 }
